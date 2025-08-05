02f9998829f37e2ea12ea25a0ed347dd2d6f9147
Angelina sisi xia <axia@sfu.ca>
2025-08-04 20:54:19 -0700

    updated review code it actually works now

--- scrape.py
+++ scrape.py
@@ -1,32 +1,46 @@
 from urllib.parse import urljoin
-import requests
+from collections import Counter
 from bs4 import BeautifulSoup
 import json
-import argparse
 from datetime import datetime
 import time
 import os
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, ElementClickInterceptedException
+import traceback
 
 driver = None
 
 # Grab a page and return the parsed BeautifulSoup object
-def fetch_page(url):
-        
-    driver.get(url)
+def fetch_page(url: str|None):
+    if url is not None:    
+        driver.get(url)
     
     return BeautifulSoup(driver.page_source, 'html.parser')
 
+def close_login() -> bool:
+    try:
+        dismiss_button = driver.find_element(By.XPATH,'//img[@alt="Dismiss"]|//button[@aria-label="Close"]')
+        dismiss_button.click()
+        return True
+
+    except (NoSuchElementException, ElementNotInteractableException):
+        return False
 
 # Scrape search results from Goodreads
 def scrape_search_results(search_url):
     soup = fetch_page(search_url)
+    
+    time.sleep(1)
+    close_login()
+
     if soup is None:
         print("Failed to get the page or parse the content.")
         return []
 
-    book_containers = soup.find_all('tr', {'itemtype': 'http://schema.org/Book'}, limit=5)
+    book_containers = soup.find_all('tr', {'itemtype': 'http://schema.org/Book'}, limit=2)
     books = []
 
     for book in book_containers:
@@ -59,28 +73,23 @@ def scrape_search_results(search_url):
 
         except Exception as e:
             print(f"Error extracting book information: {e}")
+            traceback.print_exc()
 
     return books
 
 #get genres, as it is not on main book list page but within the book url
 def get_genres(book_url):
     book_soup = fetch_page(book_url)
+    time.sleep(1)
+    close_login()
     genres = book_soup.select("div[data-testid='genresList'] a span.Button__labelItem")
     genres_text = ', '.join([g.text.strip() for g in genres])
     return genres_text
 
-#scrape the book reviews
-def scrape_book_reviews(book_url):
-    soup = fetch_page(book_url)
-
-    if soup is None:
-        return []
-
-    genres = soup.select("div[data-testid='genresList'] a span.Button__labelItem")
-    genres_text = ', '.join([g.text.strip() for g in genres])
-
-    review_containers = soup.find_all('article', class_='ReviewCard', limit=3)
+def get_reviews(soup):
     
+    review_containers = soup.find_all('article', class_='ReviewCard')
+
     reviews = []
 
     for review in review_containers:
@@ -94,21 +103,66 @@ def scrape_book_reviews(book_url):
                 rating_number = str(rating_tag['aria-label']).split(' ')[1] + "/5"
             review_text_container = review.find('span', class_='Formatted')
             review_text = review_text_container.get_text(strip=True) if review_text_container else "No review text"
-
-
+            #find the review id from the url
+            review_id_tag = review.find('span', class_='Text Text__body3')
+            reviewer_id = str(review_id_tag.a.get('href')).split('/')[-1]
             review_info = {
+                "review_id":reviewer_id,
                 "reviewer:": reviewer,
                 "rating": rating_number,
                 "review_text": review_text,
             }
+
             reviews.append(review_info)
 
         except Exception as e:
             print(f"Error extracting review:")
             print(e)
             continue
+            
+    return reviews
+
+def check_for_duplicates(reviews):
+    review_ids = [r['review_id'] for r in reviews]  
+    num_duplicates = len([_id for _id, _count in Counter(review_ids).items() if _count > 1])
+    return num_duplicates
+
+#scrape the book reviews
+def scrape_book_reviews(book_url):
+    soup = fetch_page(book_url)
+
+    time.sleep(1)
+    close_login()
+
+    if soup is None:
+        return []
+
+    reviews = get_reviews(soup)
+
+    next_link_xpath = '//span[@data-testid="loadMore"]|//a[@aria-label="Tap to show more reviews and ratings"]'
+    
+    while len(reviews) <=100: 
+        try:
+            next_link = driver.find_element(By.XPATH,next_link_xpath)
+            driver.execute_script("arguments[0].scrollIntoView(true);", next_link) #from: https://groups.google.com/g/selenium-remote-driver/c/gc60TeZPU5I
+            next_link.click()
+            time.sleep(3)
+            soup = fetch_page(None)
+            reviews.extend(get_reviews(soup))
+        except ElementNotInteractableException:
+            pass
+        except ElementClickInterceptedException:
+            pass
+        except NoSuchElementException:
+            break
+        
+    if check_for_duplicates(reviews) > 0:
+        reviews = [dict(t) for t in {tuple(sorted(d.items())) for d in reviews}] 
+        #list comprehension to remove duplicates from list of dicts by turning it into set of tuples and back 
+        # code from https://stackoverflow.com/questions/9427163/remove-duplicate-dict-in-list-in-python
 
     return reviews
+
 
 # Save data to a JSON file
 def save_book_to_json(book, index, directory='books_json'):
@@ -127,6 +181,7 @@ def main():
     service = Service('/Users/angelinasisixia1/Desktop/School/geckodriver')
     global driver
     with webdriver.Firefox(service=service) as driver:
+        #driver.implicitly_wait(3)
         search_url = f'https://www.goodreads.com/list/show/2384.Best_Gothic_Novels_Suspense_Novels'
         books = scrape_search_results(search_url)
 
